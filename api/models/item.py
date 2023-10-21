74e12310856549d8ac38c3dd459f58c31324bd16
Tiago Sora <tiagogcarvalho@ua.pt>
2023-10-21 02:58:30 +0100

    setup and endpoint scratch

---
+++ api/models/item.py
@@ -0,0 +1,10 @@
+from pydantic import BaseModel
+
+class Item(BaseModel):
+    id: int
+    description: str
+    image: str
+    video: str
+    tag: str = None
+    dropoffPoint_id: int
+    mail: str
