74e6d78b7dc8595b7665b42bc6bb8b35b3536d79
Tiago Sora <tiagogcarvalho@ua.pt>
2023-10-21 15:06:39 +0100

    set up and Get All Items endpoint UAC-44

---
+++ api/main.py
@@ -0,0 +1,28 @@
+import uvicorn
+from fastapi import FastAPI
+from api.models.item import Item
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+app = FastAPI()
+
+engine = create_engine("mysql+mysqlconnector://user:password@localhost/database") #TODO
+SessionLocal = sessionmaker(autocommit = False, autoflush = False, bind = engine)
+
+@app.get("/items/", response_model = list[Item])
+def get_all_items():
+    db = SessionLocal()
+    items = db.query(Item).all()
+    return items
+
+# @app.post("/items/add/")
+# def create_item(item: Item):
+#     db = SessionLocal()
+#     db.add(item)
+#     db.commit()
+#     db.refresh(item)
+#     return item
+
+if __name__  == '__main__':
+    uvicorn.run(app, host = 'localhost', port = 8000)
---
+++ tests/test_main.py
@@ -0,0 +1,27 @@
+import requests
+
+# Define the base URL for the FastAPI application
+BASE_URL = "http://localhost:8000"
+
+# Test GET request for all items                    #TODO MockItems
+def test_get_all_items():
+    response = requests.get(f"{BASE_URL}/items")
+    assert response.status_code == 200
+
+# Test POST request for creating an item
+# def test_create_item():
+#     data = {
+#         "description": "description1",
+#         "image": "image1.jpg",
+#         "video": "video1.mp4",
+#         "tag": "tag1",
+#         "dropoffPoint_id": 1
+#     }
+#     response = requests.post(f"{BASE_URL}/items", json=data)
+#     assert response.status_code == 200
+#     assert response.json()["description"] == data["description"]
+
+# Run the tests
+if __name__ == "__main__":
+    test_get_all_items()
+    # test_create_item()
