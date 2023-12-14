d2f0d80ccb53e4fba98fc696f685d0b4495ce665
Tiago Sora <tiagogcarvalho@ua.pt>
2023-12-14 18:28:05 +0000

    issues solving

--- api/db_info/init_db.py
+++ api/db_info/init_db.py
@@ -4,7 +4,7 @@ from db_info import models
 
 def init(db: Session):
     
-    description = 'description_opt'
+    description = "description_opt"
     tags = {
         "auscultadoresfones": "Auscultadores/Fones",
         "carregadores": "Carregadores",
@@ -12,47 +12,53 @@ def init(db: Session):
         "tablets": "Tablets",
         "telemoveis": "Telemóveis",
     }
-    
+    images = {
+        "auscultadores": "auscultadores.jpeg",
+        "carregador": "carregador.jpeg",
+        "portateis": "portateis.jpeg",
+        "tablet": "tablet.jpeg",
+        "telemovel": "telemovel.jpeg",
+    }
     states = {
         "stored": "stored"
     }
 
     initial_items_on_db = [
-        models.Item(description='description_opt', tag=tags["tablets"], image=None, state=states["stored"], dropoff_point_id=1, report_email=None, retrieved_email=None, retrieved_date=None),
-        models.Item(description='description_opt', tag=tags["carregadores"], image='carregador.jpeg', state=states["stored"], dropoff_point_id=1, report_email=None, retrieved_email=None, retrieved_date=None),
-        models.Item(description='description_opt', tag=tags["carregadores"], image='carregador.jpeg', state=states["stored"], dropoff_point_id=1, report_email=None, retrieved_email=None, retrieved_date=None),
-        models.Item(description='description_opt', tag=tags["telemoveis"], image=None, state=states["stored"], dropoff_point_id=1, report_email=None, retrieved_email=None, retrieved_date=None),
-        models.Item(description='description_opt', tag=tags["tablets"], image=None, state=states["stored"], dropoff_point_id=1, report_email=None, retrieved_email=None, retrieved_date=None),
-        models.Item(description='description_opt', tag=tags["telemoveis"], image=None, state=states["stored"], dropoff_point_id=1, report_email=None, retrieved_email=None, retrieved_date=None),
-        models.Item(description='description_opt', tag=tags["telemoveis"], image='telemovel.jpeg', state=states["stored"], dropoff_point_id=1, report_email=None, retrieved_email=None, retrieved_date=None),
-        models.Item(description='description_opt', tag=tags["telemoveis"], image='telemovel.jpeg', state=states["stored"], dropoff_point_id=1, report_email=None, retrieved_email=None, retrieved_date=None),
-        models.Item(description='description_opt', tag=tags["carregadores"], image='carregador.jpeg', state=states["stored"], dropoff_point_id=1, report_email=None, retrieved_email=None, retrieved_date=None),
-        models.Item(description='description_opt', tag=tags["auscultadoresfones"], image='auscultadores.jpeg', state=states["stored"], dropoff_point_id=1, report_email=None, retrieved_email=None, retrieved_date=None),
-        models.Item(description='description_opt', tag=tags["telemoveis"], image=None, state=states["stored"], dropoff_point_id=1, report_email=None, retrieved_email=None, retrieved_date=None),
-        models.Item(description='description_opt', tag=tags["portateis"], image=None, state=states["stored"], dropoff_point_id=1, report_email=None, retrieved_email=None, retrieved_date=None),
-        models.Item(description='description_opt', tag=tags["tablets"], image='tablet.jpeg', state=states["stored"], dropoff_point_id=1, report_email=None, retrieved_email=None, retrieved_date=None),
-        models.Item(description='description_opt', tag=tags["auscultadoresfones"], image=None, state=states["stored"], dropoff_point_id=1, report_email=None, retrieved_email=None, retrieved_date=None),
-        models.Item(description='description_opt', tag=tags["portateis"], image='portateis.jpeg', state=states["stored"], dropoff_point_id=1, report_email=None, retrieved_email=None, retrieved_date=None),
-        models.Item(description='description_opt', tag=tags["auscultadoresfones"], image=None, state=states["stored"], dropoff_point_id=1, report_email=None, retrieved_email=None, retrieved_date=None),
-        models.Item(description='description_opt', tag=tags["carregadores"], image=None, state=states["stored"], dropoff_point_id=1, report_email=None, retrieved_email=None, retrieved_date=None),
-        models.Item(description='description_opt', tag=tags["tablets"], image='tablet.jpeg', state=states["stored"], dropoff_point_id=1, report_email=None, retrieved_email=None, retrieved_date=None),
-        models.Item(description='description_opt', tag=tags["telemoveis"], image=None, state=states["stored"], dropoff_point_id=1, report_email=None, retrieved_email=None, retrieved_date=None),
-        models.Item(description='description_opt', tag=tags["auscultadoresfones"], image=None, state=states["stored"], dropoff_point_id=1, report_email=None, retrieved_email=None, retrieved_date=None),
-        models.Item(description='description_opt', tag=tags["telemoveis"], image=None, state=states["stored"], dropoff_point_id=1, report_email=None, retrieved_email=None, retrieved_date=None),
-        models.Item(description='description_opt', tag=tags["auscultadoresfones"], image=None, state=states["stored"], dropoff_point_id=1, report_email=None, retrieved_email=None, retrieved_date=None),
-        models.Item(description='description_opt', tag=tags["tablets"], image='tablet.jpeg', state=states["stored"], dropoff_point_id=1, report_email=None, retrieved_email=None, retrieved_date=None),
-        models.Item(description='description_opt', tag=tags["auscultadoresfones"], image='auscultadores.jpeg', state=states["stored"], dropoff_point_id=1, report_email=None, retrieved_email=None, retrieved_date=None),
-        models.Item(description='description_opt', tag=tags["auscultadoresfones"], image=None, state=states["stored"], dropoff_point_id=1, report_email=None, retrieved_email=None, retrieved_date=None),
-        models.Item(description='description_opt', tag=tags["auscultadoresfones"], image=None, state=states["stored"], dropoff_point_id=1, report_email=None, retrieved_email=None, retrieved_date=None),
-        models.Item(description='description_opt', tag=tags["auscultadoresfones"], image='auscultadores.jpeg', state=states["stored"], dropoff_point_id=1, report_email=None, retrieved_email=None, retrieved_date=None),
-        models.Item(description='description_opt', tag=tags["auscultadoresfones"], image='auscultadores.jpeg', state=states["stored"], dropoff_point_id=1, report_email=None, retrieved_email=None, retrieved_date=None),
-        models.Item(description='description_opt', tag=tags["auscultadoresfones"], image=None, state=states["stored"], dropoff_point_id=1, report_email=None, retrieved_email=None, retrieved_date=None),
-        models.Item(description='description_opt', tag=tags["auscultadoresfones"], image=None, state=states["stored"], dropoff_point_id=1, report_email=None, retrieved_email=None, retrieved_date=None),
-        models.Item(description='description_opt', tag=tags["auscultadoresfones"], image=None, state=states["stored"], dropoff_point_id=1, report_email=None, retrieved_email=None, retrieved_date=None),
-        models.Item(description='description_opt', tag=tags["carregadores"], image=None, state=states["stored"], dropoff_point_id=1, report_email=None, retrieved_email=None, retrieved_date=None),
-        models.Item(description='description_opt', tag=tags["tablets"], image='tablet.jpeg', state=states["stored"], dropoff_point_id=1, report_email=None, retrieved_email=None, retrieved_date=None),
-        models.Item(description='description_opt', tag=tags["portateis"], image=None, state=states["stored"], dropoff_point_id=1, report_email=None, retrieved_email=None, retrieved_date=None),
-        models.Item(description='description_opt', tag=tags["tablets"], image='tablet.jpeg', state=states["stored"], dropoff_point_id=1, report_email=None, retrieved_email=None, retrieved_date=None),
+        models.Item(description=description, tag=tags["tablets"], image=None, state=states["stored"], dropoff_point_id=1, report_email=None, retrieved_email=None, retrieved_date=None),
+        models.Item(description=description, tag=tags["carregadores"], image=images["carregador"], state=states["stored"], dropoff_point_id=1, report_email=None, retrieved_email=None, retrieved_date=None),
+        models.Item(description=description, tag=tags["carregadores"], image=images["carregador"], state=states["stored"], dropoff_point_id=1, report_email=None, retrieved_email=None, retrieved_date=None),
+        models.Item(description=description, tag=tags["telemoveis"], image=None, state=states["stored"], dropoff_point_id=1, report_email=None, retrieved_email=None, retrieved_date=None),
+        models.Item(description=description, tag=tags["tablets"], image=None, state=states["stored"], dropoff_point_id=1, report_email=None, retrieved_email=None, retrieved_date=None),
+        models.Item(description=description, tag=tags["telemoveis"], image=None, state=states["stored"], dropoff_point_id=1, report_email=None, retrieved_email=None, retrieved_date=None),
+        models.Item(description=description, tag=tags["telemoveis"], image=images["telemovel"], state=states["stored"], dropoff_point_id=1, report_email=None, retrieved_email=None, retrieved_date=None),
+        models.Item(description=description, tag=tags["telemoveis"], image=images["telemovel"], state=states["stored"], dropoff_point_id=1, report_email=None, retrieved_email=None, retrieved_date=None),
+        models.Item(description=description, tag=tags["carregadores"], image=images["carregador"], state=states["stored"], dropoff_point_id=1, report_email=None, retrieved_email=None, retrieved_date=None),
+        models.Item(description=description, tag=tags["auscultadoresfones"], image=images["auscultadores"], state=states["stored"], dropoff_point_id=1, report_email=None, retrieved_email=None, retrieved_date=None),
+        models.Item(description=description, tag=tags["telemoveis"], image=None, state=states["stored"], dropoff_point_id=1, report_email=None, retrieved_email=None, retrieved_date=None),
+        models.Item(description=description, tag=tags["portateis"], image=None, state=states["stored"], dropoff_point_id=1, report_email=None, retrieved_email=None, retrieved_date=None),
+        models.Item(description=description, tag=tags["tablets"], image=images["tablet"], state=states["stored"], dropoff_point_id=1, report_email=None, retrieved_email=None, retrieved_date=None),
+        models.Item(description=description, tag=tags["auscultadoresfones"], image=None, state=states["stored"], dropoff_point_id=1, report_email=None, retrieved_email=None, retrieved_date=None),
+        models.Item(description=description, tag=tags["portateis"], image=images["portateis"], state=states["stored"], dropoff_point_id=1, report_email=None, retrieved_email=None, retrieved_date=None),
+        models.Item(description=description, tag=tags["auscultadoresfones"], image=None, state=states["stored"], dropoff_point_id=1, report_email=None, retrieved_email=None, retrieved_date=None),
+        models.Item(description=description, tag=tags["carregadores"], image=None, state=states["stored"], dropoff_point_id=1, report_email=None, retrieved_email=None, retrieved_date=None),
+        models.Item(description=description, tag=tags["tablets"], image=images["tablet"], state=states["stored"], dropoff_point_id=1, report_email=None, retrieved_email=None, retrieved_date=None),
+        models.Item(description=description, tag=tags["telemoveis"], image=None, state=states["stored"], dropoff_point_id=1, report_email=None, retrieved_email=None, retrieved_date=None),
+        models.Item(description=description, tag=tags["auscultadoresfones"], image=None, state=states["stored"], dropoff_point_id=1, report_email=None, retrieved_email=None, retrieved_date=None),
+        models.Item(description=description, tag=tags["telemoveis"], image=None, state=states["stored"], dropoff_point_id=1, report_email=None, retrieved_email=None, retrieved_date=None),
+        models.Item(description=description, tag=tags["auscultadoresfones"], image=None, state=states["stored"], dropoff_point_id=1, report_email=None, retrieved_email=None, retrieved_date=None),
+        models.Item(description=description, tag=tags["tablets"], image=images["tablet"], state=states["stored"], dropoff_point_id=1, report_email=None, retrieved_email=None, retrieved_date=None),
+        models.Item(description=description, tag=tags["auscultadoresfones"], image=images["auscultadores"], state=states["stored"], dropoff_point_id=1, report_email=None, retrieved_email=None, retrieved_date=None),
+        models.Item(description=description, tag=tags["auscultadoresfones"], image=None, state=states["stored"], dropoff_point_id=1, report_email=None, retrieved_email=None, retrieved_date=None),
+        models.Item(description=description, tag=tags["auscultadoresfones"], image=None, state=states["stored"], dropoff_point_id=1, report_email=None, retrieved_email=None, retrieved_date=None),
+        models.Item(description=description, tag=tags["auscultadoresfones"], image=images["auscultadores"], state=states["stored"], dropoff_point_id=1, report_email=None, retrieved_email=None, retrieved_date=None),
+        models.Item(description=description, tag=tags["auscultadoresfones"], image=images["auscultadores"], state=states["stored"], dropoff_point_id=1, report_email=None, retrieved_email=None, retrieved_date=None),
+        models.Item(description=description, tag=tags["auscultadoresfones"], image=None, state=states["stored"], dropoff_point_id=1, report_email=None, retrieved_email=None, retrieved_date=None),
+        models.Item(description=description, tag=tags["auscultadoresfones"], image=None, state=states["stored"], dropoff_point_id=1, report_email=None, retrieved_email=None, retrieved_date=None),
+        models.Item(description=description, tag=tags["auscultadoresfones"], image=None, state=states["stored"], dropoff_point_id=1, report_email=None, retrieved_email=None, retrieved_date=None),
+        models.Item(description=description, tag=tags["carregadores"], image=None, state=states["stored"], dropoff_point_id=1, report_email=None, retrieved_email=None, retrieved_date=None),
+        models.Item(description=description, tag=tags["tablets"], image=images["tablet"], state=states["stored"], dropoff_point_id=1, report_email=None, retrieved_email=None, retrieved_date=None),
+        models.Item(description=description, tag=tags["portateis"], image=None, state=states["stored"], dropoff_point_id=1, report_email=None, retrieved_email=None, retrieved_date=None),
+        models.Item(description=description, tag=tags["tablets"], image=images["tablet"], state=states["stored"], dropoff_point_id=1, report_email=None, retrieved_email=None, retrieved_date=None),
     ]
 
     for entry in initial_items_on_db:
